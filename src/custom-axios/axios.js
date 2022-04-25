c891d65c8fa2c56de04a6884398a621a0f3c3dba
Igor Ognjanov <igorognjanov123@gmail.com>
2022-04-25 11:17:17 +0200

    fix

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://warm-ridge-02857.herokuapp.com:80/api',
+    baseURL: 'https://warm-ridge-02857.herokuapp.com/api',
     headers: {
         'Access-Control-Allow-Origin': '*'
     }
